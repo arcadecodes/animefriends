b62d16b7a46c9019c48d5e791543ccbd61477b7a
Arcade Kwaku <arcade.kwaku@gmail.com>
2020-09-14 20:34:50 +0100

    creates search function for app

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--pink bg-lightest-pink' type="search" placeholder="search characters" />
+            <input className='pa3 ba b--pink bg-lightest-pink'
+                type="search"
+                placeholder="search characters"
+                onChange={searchChange} />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList'
+//import CardList from './CardList'
 //standard that it is capitalised
 
 import * as serviceWorker from './serviceWorker';
