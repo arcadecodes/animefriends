abe3a10bc38147bf772eaa514d6568211c580bf8
Arcade Kwaku <arcade.kwaku@gmail.com>
2020-10-04 10:05:23 +0100

    Updates

---
+++ precache-manifest.bbf5436880bddc8d8797c79b4b989b85.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9720d8278c2cac352a4e9c5f91666b51",
+    "url": "/animefriends/index.html"
+  },
+  {
+    "revision": "b3a44722222e45987bdc",
+    "url": "/animefriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c4bb500d2bb54bf65378",
+    "url": "/animefriends/static/css/main.80151dc5.chunk.css"
+  },
+  {
+    "revision": "b3a44722222e45987bdc",
+    "url": "/animefriends/static/js/2.a22909c7.chunk.js"
+  },
+  {
+    "revision": "fc9bf2d633f142820ff84625bb729377",
+    "url": "/animefriends/static/js/2.a22909c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bb500d2bb54bf65378",
+    "url": "/animefriends/static/js/main.29a87661.chunk.js"
+  },
+  {
+    "revision": "8b1dc8f4a3e7e134a099",
+    "url": "/animefriends/static/js/runtime-main.d8aeb56d.js"
+  },
+  {
+    "revision": "7d2f906055274bd649694b9be276dd92",
+    "url": "/animefriends/static/media/JapaneseBrush.7d2f9060.woff"
+  },
+  {
+    "revision": "98021f3a7f8861777592c127c4e4f9a4",
+    "url": "/animefriends/static/media/JapaneseBrush.98021f3a.woff2"
+  },
+  {
+    "revision": "21c9ccf842e4ff64ad5581f80c602d83",
+    "url": "/animefriends/static/media/YellowRabbit-PersonalUse.21c9ccf8.woff"
+  },
+  {
+    "revision": "946a108e8c3ad410ca99c413916a8fea",
+    "url": "/animefriends/static/media/YellowRabbit-PersonalUse.946a108e.woff2"
+  },
+  {
+    "revision": "d69c5738413c5b6a544a71241abf8f42",
+    "url": "/animefriends/static/media/aisaka.d69c5738.png"
+  },
+  {
+    "revision": "dd7abb4a5288a8f9d8753e0be1c09d43",
+    "url": "/animefriends/static/media/ban.dd7abb4a.png"
+  },
+  {
+    "revision": "3d5489ddcb2e89a7dda2fecd81336edb",
+    "url": "/animefriends/static/media/edward.3d5489dd.png"
+  },
+  {
+    "revision": "b3d2b9752256ccc17710b6a6f55a9843",
+    "url": "/animefriends/static/media/emilia.b3d2b975.png"
+  },
+  {
+    "revision": "3eb7ed4cd52dc974a83180f03346420e",
+    "url": "/animefriends/static/media/mikasa.3eb7ed4c.png"
+  },
+  {
+    "revision": "71a951874c9269827b3d9fcd6e8f2fac",
+    "url": "/animefriends/static/media/mikasa.71a95187.jpg"
+  },
+  {
+    "revision": "349542e98b24db9ffbd9f56557b9fc0c",
+    "url": "/animefriends/static/media/ryuko.349542e9.jpg"
+  },
+  {
+    "revision": "47424e1698155fe2fc6ae4ea4bbb2855",
+    "url": "/animefriends/static/media/ryuko.47424e16.png"
+  },
+  {
+    "revision": "dfb062fdad18ba222994edae4679e104",
+    "url": "/animefriends/static/media/satsuki.dfb062fd.jpg"
+  },
+  {
+    "revision": "2c713804b5a207acb94c4284cf23f339",
+    "url": "/animefriends/static/media/shoto.2c713804.png"
+  }
+]);
